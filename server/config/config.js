ce11582b0259b031bf00d8834d178759addee70d
Yordi <yordicartagena24@gmail.com>
2018-10-04 19:59:18 -0400

    RestServer - config fix

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb://<cafe-user>:124578963Rr@ds223653.mlab.com:23653/cafe';
+    urlDB = 'mongodb://cafe-user:124578963Rr@ds223653.mlab.com:23653/cafe';
 }
 
 process.env.URL_DB = urlDB;
